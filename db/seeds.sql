b39f35ba7886361865a9e4f8bd99b1fa6c0e30d6
John Brown <john.brown@libertymutual.com>
2019-04-27 23:02:41 -0400

    added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spam Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
